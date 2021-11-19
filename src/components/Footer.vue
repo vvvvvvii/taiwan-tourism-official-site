639b7a868700b38c19acb2bcde0fa17ea53389ba
圈圈 <vvvvvvii@yahoo.com.tw>
2021-11-19 12:07:38 +0800

    footer rwd ok

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="bg-primary footer">
-    <div class="d-flex justify-content-between w-50 mx-auto mb-10">
-      <a href="#" class="text-dark">關於 Taiwan GO</a>
+    <a href="#" class="text-dark d-md-none fs-xxs text-center mb-10">關於 Taiwan GO</a>
+    <div class="d-flex justify-content-between w-lg-50 w-75 mx-auto mb-10">
+      <a href="#" class="text-dark fs-md-sm fs-xxs d-md-inline d-none">關於 Taiwan GO</a>
       <ul>
-        <li><a href="#" class="text-dark mb-7">投稿說明</a></li>
-        <li><a href="#" class="text-dark mb-7">投稿說明</a></li>
-        <li><a href="#" class="text-dark">版權所有</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs mb-7">投稿說明</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs mb-7">投稿說明</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs">版權所有</a></li>
       </ul>
       <ul>
-        <li><a href="#" class="text-dark mb-7">最新消息</a></li>
-        <li><a href="#" class="text-dark mb-7">展覽資訊</a></li>
-        <li><a href="#" class="text-dark">景點告示</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs mb-7">最新消息</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs mb-7">展覽資訊</a></li>
+        <li><a href="#" class="text-dark fs-md-sm fs-xxs">景點告示</a></li>
       </ul>
-      <a href="#" class="text-dark">友善旅遊</a>
-      <a href="#" class="text-dark">活動行事曆</a>
+      <a href="#" class="text-dark fs-md-sm fs-xxs">友善旅遊</a>
+      <a href="#" class="text-dark fs-md-sm fs-xxs">活動行事曆</a>
     </div>
     <div class="container">
-      <router-link to="/" title="回到首頁" class="mb-6">
-        <img
-          src="https://github.com/vvvvvvii/taiwan-tourism-official-site/blob/main/public/img/logo.png?raw=true"
-          alt="footer-logo"
-        />
-      </router-link>
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between align-items-center mb-6">
+        <router-link to="/" title="回到首頁">
+          <img
+            src="https://github.com/vvvvvvii/taiwan-tourism-official-site/blob/main/public/img/logo.png?raw=true"
+            alt="footer-logo"
+          />
+        </router-link>
+        <a href="#" class="d-md-none fs-xxs">
+          關注我
+          <i class="bi bi-facebook ms-md-4 ms-2"></i>
+        </a>
+      </div>
+      <div class="d-flex justify-content-between align-items-center">
         <div class="d-flex">
-          <p class="fs-xxs">Design 芬妮</p>
-          <p class="ms-5 fs-xxs">更新日期：2021.11.03</p>
-          <p class="ms-5 fs-xxs">圖片來源：https://unsplash.com/</p>
+          <p class="fs-md-xxs fs-xxxs">Design 芬妮</p>
+          <p class="ms-5 fs-md-xxs fs-xxxs">更新日期：2021.11.03</p>
+          <p class="ms-5 fs-md-xxs fs-xxxs">圖片來源：https://unsplash.com/</p>
         </div>
-        <a href="#">
+        <a href="#" class="d-md-block d-none">
           關注我
           <i class="bi bi-facebook ms-4"></i>
         </a>
